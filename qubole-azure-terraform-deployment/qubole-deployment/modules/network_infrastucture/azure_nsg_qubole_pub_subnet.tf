b34e2dc0273f171fcf57772d737baecae1b92b33
vihag <vihag.gupta@gmail.com>
2020-09-24 22:17:02 +0800

    removed tunnel hardcoding

--- qubole-azure-terraform-deployment/qubole-deployment/modules/network_infrastucture/azure_nsg_qubole_pub_subnet.tf
+++ qubole-azure-terraform-deployment/qubole-deployment/modules/network_infrastucture/azure_nsg_qubole_pub_subnet.tf
@@ -17,14 +17,7 @@ resource "azurerm_network_security_rule" "qubole_tunnel_ingress_ssh_subnet" {
   source_port_range = "*"
   destination_port_range = "22"
   #Includes US-SOC addresses
-  source_address_prefixes = [
-    "54.164.240.44/32",
-    "52.23.20.1/32",
-    "40.121.60.130/32",
-    "40.114.27.47/32",
-    "13.82.198.90/32",
-    "52.44.223.209/32",
-    "100.25.6.193/32"]
+  source_address_prefixes = var.qubole_tunnel_nat
   destination_address_prefixes = [azurerm_public_ip.qubole_bastion_host_public_ip.ip_address, azurerm_network_interface.qubole_vnet_nic.private_ip_address]
   resource_group_name = var.qubole_resource_group_name
   network_security_group_name = azurerm_network_security_group.qubole_pub_subnet_security_group.name
--- qubole-azure-terraform-deployment/qubole-deployment/modules/network_infrastucture/variables.tf
+++ qubole-azure-terraform-deployment/qubole-deployment/modules/network_infrastucture/variables.tf
@@ -13,6 +13,11 @@ variable "public_ssh_key" {
 variable "qubole_public_key" {
 }
 
+variable "qubole_tunnel_nat" {
+  type    = list(string)
+  default = ["54.164.240.44/32","52.23.20.1/32","40.121.60.130/32","40.114.27.47/32","13.82.198.90/32","52.44.223.209/32","100.25.6.193/32"]
+}
+
 variable "bastion_vm_size" {
   default = "Standard_DS1_v2"
 }
