866e9501cf9a21511424ca05b9f45b6e82690e43
nunyawizzy <31305238+nunyawizzy@users.noreply.github.com>
2025-04-15 23:18:47 +0000

    Update aws_eip_qubole_vpc_nat_gw.tf, aws_eip updated with vpc attribute changed to domain

--- qubole-aws-terraform-deployment/qubole-deployment/modules/network_infrastucture/aws_eip_qubole_vpc_nat_gw.tf
+++ qubole-aws-terraform-deployment/qubole-deployment/modules/network_infrastucture/aws_eip_qubole_vpc_nat_gw.tf
@@ -3,7 +3,7 @@ Creates a Elastic IP address for
  1. The NAT Gateway running in the public subnet of the Qubole dedicated VPC
 */
 resource "aws_eip" "qubole_dedicated_vpc_nat_gw_eip" {
-  vpc      = true
+  domain   = "vpc"
   tags = {
     Name = "qubole_dedicated_vpc_nat_gw_eip_${var.deployment_suffix}"
   }
